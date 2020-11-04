e271c59db108f3cb3be9b9e7880298b60a6645b3
lidonghao <leedonhom@gmail.com>
2020-11-04 15:41:53 -0500

    update 10.27

--- mlcTest.py
+++ mlcTest.py
@@ -176,7 +176,7 @@ table3 = dataset(data3, data3_label)
 
 test = [table1, table2, table3]
 name = [data1, data2, data3]
-time = "test5"
+time = "test6"
 
 for i in range(len(test)):
     test_results = get_training(test[i], 10, name[i], time)
