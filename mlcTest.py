e02125894d70c7ede60e894a30a322f8efa4cd8c
lidonghao <leedonhom@gmail.com>
2020-11-07 21:19:22 -0500

    update 10.27

--- mlcTest.py
+++ mlcTest.py
@@ -169,8 +169,8 @@ data1 = "intermediate"
 data1_label = "intermediate_label"
 data2 = "hard"
 data2_label = "hard_label"
-data3 = "data_label"
-data3_label = "data"
+data3 = "data"
+data3_label = "data_label"
 
 table1 = dataset(data1, data1_label)
 table2 = dataset(data2, data2_label)
@@ -181,7 +181,7 @@ file_diretory = "converge_test_hard"
 for i in range(0,10):
     i = str(i+1)
     data = data3 + i
-    data_label = data3 + i
+    data_label = data3_label + i
     table = dataset(data, data_label)
     cf, result2 = get_training(table, 20, file_diretory, data1)
     array = []
