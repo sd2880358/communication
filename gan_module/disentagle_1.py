7570ab52f52bda78a35dddece353f365cbf5b56f
lidonghao <leedonhom@gmail.com>
2020-11-19 14:22:45 -0500

    update 10.27

---
+++ gan_module/disentagle_1.py
@@ -0,0 +1,214 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+import os,sys
+import scipy.io as sc
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers.experimental import preprocessing
+import time
+from IPython.display import clear_output
+import math
+import mlcTest as mt
+def dataset(dataFile, labelFile):
+    dataFile = "./communication/" + dataFile
+    labelFile = "./communication/" + labelFile
+    my_data = sc.loadmat(dataFile)
+    my_labels = sc.loadmat(labelFile)
+    my_data = my_data['Y']
+    X = my_labels['L_S_x']
+    myOrig = table_data(my_data, my_labels['L_Constellations'][0], X)
+    mytable = assign_label(myOrig)
+    return mytable
+
+
+def assign_label(data):
+    c_4 = [1,-1]
+    c_16 = [3,1,-1,-3]
+    c_16r = [-3,-1,1,3]
+    cons_4 = np.dot(np.sqrt(0.5),[complex(i,j)for i in c_4 for j in c_4])
+    cons_16 = np.array([complex(i,j)for j in c_16 for i in c_16r])
+    cons_16 = cons_16/np.sqrt(np.mean(np.abs(cons_16)**2))
+    cons4 = data[data.cons==1]
+    cons4_label = np.array([[cons_4[i-1]]for i in cons4.label])
+    cons16 = data[data.cons==2]
+    cons16_label = np.array([[cons_16[i-1]]for i in cons16.label.to_numpy().real.astype(int)])
+    data[data.cons==2].index
+    data['buffer'] = 0
+    data['buffer'] = 0
+    data.iloc[data[data.cons==1].index, 5] = cons4_label
+    data.iloc[data[data.cons==2].index, 5] = cons16_label
+    data['label_real'] = data.buffer.to_numpy().real
+    data['label_imag'] = data.buffer.to_numpy().imag
+    myTest = data.copy()
+    myTest.loc[myTest.cons == 2, 'label'] = myTest.loc[myTest.cons == 2, 'label'] + 4
+    myTest.label = myTest.label - 1
+    return myTest
+
+
+def table_data(my_data, cons, label):
+    block = my_data.shape[1]
+    my_data_size = my_data.shape[0] * block
+    my_data_div = my_data.T.reshape(my_data_size, )
+    cons_array = np.array([[cons[i]] * my_data.shape[0] for i in range(0, block)]).reshape(my_data_size, )
+    block_array = np.array([([i + 1] * my_data.shape[0]) for i in range(0, block)]).reshape(my_data_size, )
+    label_array = label.T.reshape(my_data_size, )
+    test_pd = pd.DataFrame({'real': my_data_div.real, 'imag': my_data_div.imag,
+                            'cons': cons_array, 'block': block_array,
+                            'label': label_array})
+    return test_pd
+
+def generator():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(20, use_bias=False, input_shape=[2,]))
+    model.add(layers.Dense(50, activation='relu'))
+    model.add(layers.Dense(2))
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(20, use_bias=False, input_shape=[2,]))
+    model.add(layers.Dense(50, activation = 'sigmoid'))
+    model.add(layers.Dense(1))
+    return model
+
+
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+def identity_loss(real, fake):
+    loss = tf.reduce_mean(tf.abs(real - fake))
+    return LAMBDA * 0.5 * loss
+
+
+@tf.function
+def train_step(total, label):
+    with tf.GradientTape(persistent=True) as tape:
+        s = generator_s(total, training=True)
+        n = generator_n(total, training=True)
+        i = generator_i(total, training=True)
+        gen = s + n + i
+        fake_t = discriminator_t(gen, training=True)
+        real_t = discriminator_t(total, training=True)
+        fake_d = discriminator_d(s, training=True)
+        real_d = discriminator_d(label, training=True)
+        s_loss = generator_loss(s)
+        n_loss = generator_loss(n)
+        i_loss = generator_loss(i)
+        disc_t_loss = discriminator_loss(real_t, fake_t)
+        disc_d_loss = discriminator_loss(real_d, fake_d)
+        identity_s_loss = identity_loss(label, s)
+        identity_g_loss = identity_loss(total, gen)
+        total_s_loss = 0.5 * (identity_s_loss + s_loss) + 0.5 * (identity_g_loss)
+        total_n_loss = identity_g_loss + n_loss
+        total_i_loss = identity_g_loss + i_loss
+        print()
+
+    gradients_of_s_generator = tape.gradient(total_s_loss, generator_s.trainable_variables)
+    gradients_of_i_generator = tape.gradient(total_i_loss, generator_i.trainable_variables)
+    gradients_of_n_generator = tape.gradient(total_n_loss, generator_n.trainable_variables)
+    gradients_of_discriminator_t = tape.gradient(disc_t_loss, discriminator_t.trainable_variables)
+    gradients_of_discriminator_d = tape.gradient(disc_d_loss, discriminator_d.trainable_variables)
+    generator_s_optimizer.apply_gradients(zip(gradients_of_s_generator, generator_s.trainable_variables))
+    generator_i_optimizer.apply_gradients(zip(gradients_of_i_generator, generator_i.trainable_variables))
+    generator_n_optimizer.apply_gradients(zip(gradients_of_n_generator, generator_n.trainable_variables))
+    discriminator_t_optimizer.apply_gradients(zip(gradients_of_discriminator_t, discriminator_t.trainable_variables))
+    discriminator_d_optimizer.apply_gradients(zip(gradients_of_discriminator_d, discriminator_d.trainable_variables))
+
+def shuffle_data(my_table):
+    data.sample(frac=1)
+    train_feature = data.loc[:, ('real', 'imag')]
+    train_label = data.loc[:, ('label_real', 'label_imag')]
+    test_feature = tf.cast(train_feature, tf.float32)
+    test_label = tf.cast(train_label, tf.float32)
+    symbol = data.loc[:, 'label']
+    return test_feature, test_label, symbol
+
+generator_s = generator()
+generator_n = generator()
+generator_i = generator()
+discriminator_t = make_discriminator_model()
+discriminator_d = make_discriminator_model()
+
+
+generator_s_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+generator_n_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+generator_i_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+discriminator_d_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_t_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_path = "./checkpoints/dis"
+
+ckpt = tf.train.Checkpoint(generator_s=generator_s,
+                           generator_n=generator_n,
+                           generator_i=generator_i,
+                           discriminator_t=discriminator_t,
+                           discriminator_d=discriminator_d,
+                           generator_s_optimizer=generator_s_optimizer,
+                           generator_n_optimizer=generator_n_optimizer,
+                           generator_i_optimizer=generator_i_optimizer,
+                           discriminator_d_optimizer=discriminator_d_optimizer,
+                           discriminator_t_optimizer=discriminator_t_optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+# if a checkpoint exists, restore the latest checkpoint.
+if ckpt_manager.latest_checkpoint:
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print ('Latest checkpoint restored!!')
+
+LAMBDA = 10
+EPOCHS = 50
+data1 = "hard"
+data1_label = "hard_label"
+data = dataset(data1, data1_label)
+file_directory = './result/tes1/'
+for epoch in range(EPOCHS):
+    test_feature, test_label, symbol = shuffle_data(data)
+    start = time.time()
+    n = 0
+
+    for i in range(len(test_feature)):
+        test = tf.reshape(test_feature[i], [1,2])
+        label = tf.reshape(test_label[i], [1,2])
+        train_step(test, label)
+        if n % 10 == 0:
+            print ('.', end='')
+            n+=1
+    if (epoch + 1) % 5 == 0:
+        ckpt_save_path = ckpt_manager.save()
+        print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
+                                                            ckpt_save_path))
+
+    if (epoch + 1) % 20 == 0:
+        s = generator_s(test_feature, training=False)
+        i = generator_i(test_feature, training=False)
+        n = generator_n(test_feature, training=False)
+        gen = s + i + n
+        test = np.array([(gen - test_feature)])
+        signal_loss = np.array([s - test_label]).mean()
+        print('The generator total loss is', test.mean())
+        print('The signal loss is ', signal_loss.mean())
+        real = np.array([s[:, 0]])
+        imag = np.array([s[:, 1]])
+        size = real.size
+        mlcData = pd.DataFrame({'real':real.reshape(size), 'imag':imag.reshape(size), 'label':symbol})
+        cf_ori, test_result = mt.get_train(mlcData, 20)
+        if not os.path.exists(file_directory):
+            os.makedirs(file_directory)
+        cf_ori.to_csv(file_directory + '/test1' + epoch, index=False)
+        cf_new = mt.batch_result(cf_ori)
+        cf_new.to_csv(file_directory + '/batch' + epoch, index=False)
+        test_result = pd.DataFrame(test_result)
+        test_result.to_csv(file_directory+'result' + epoch, index=False)
+
---
+++ gan_module/mlcTest.py
@@ -0,0 +1,236 @@
+import tensorflow as tf
+import numpy as np
+'import matplotlib.pyplot as plt'
+import scipy.io as sc
+import pandas as pd
+import seaborn as sns
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+
+
+
+
+def dataset(dataFile, labelFile):
+    dataFile = "./communication/" + dataFile
+    labelFile = "./communication/" + labelFile
+    my_data = sc.loadmat(dataFile)
+    my_labels = sc.loadmat(labelFile)
+    my_data = my_data['Y']
+    X = my_labels['L_S_x']
+    myOrig = table_data(my_data, my_labels['L_Constellations'][0], X)
+    myTable = assign_labels(myOrig)
+    return myTable
+
+
+def table_data(my_data, cons, label):
+    block = my_data.shape[1]
+    my_data_size = my_data.shape[0] * block
+    my_data_div = my_data.T.reshape(my_data_size, )
+    cons_array = np.array([[cons[i]] * my_data.shape[0] for i in range(0, block)]).reshape(my_data_size, )
+    block_array = np.array([([i + 1] * my_data.shape[0]) for i in range(0, block)]).reshape(my_data_size, )
+    label_array = label.T.reshape(my_data_size, )
+    test_pd = pd.DataFrame({'real': my_data_div.real, 'imag': my_data_div.imag,
+                            'cons': cons_array, 'block': block_array,
+                            'label': label_array})
+    return test_pd
+
+
+def assign_labels(myTable):
+    myTest = myTable.copy()
+    myTest.loc[myTest.cons == 2, 'label'] = myTest.loc[myTest.cons == 2, 'label'] + 4
+    myTest.label = myTest.label - 1
+    return myTest
+
+
+from numpy.random import default_rng
+
+
+def training_set(myTable):
+    block = myTable.shape[0]
+    rng = default_rng()
+    sample_size = int(0.8 * block)
+    numbers = rng.choice(range(1, block + 1), size=sample_size, replace=False)
+    training_dataset = myTable[myTable.block.isin(numbers)]
+    return training_dataset
+
+def get_results(results):
+    prediction_results = []
+    for i in range(len(results)):
+        prediction_results.append(np.argmax(results[i]))
+    return prediction_results
+
+def test_set(myTable, training_dataset):
+    remaining = myTable.drop(training_dataset.index)
+    return remaining
+
+
+def build_and_compile_model(norm):
+    model = keras.Sequential([
+        norm,
+        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(20)
+    ])
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  optimizer=tf.keras.optimizers.Adam(0.01),
+                  metrics=['accuracy'])
+    return model
+
+'''
+def get_total_loss(predict_label, true_label):
+    i = 0
+    for j in range(len(predict_label)):
+        prediction = np.argmax(predict_label[j])
+        if prediction != true_label[j]:
+            i = i + test1
+    return i / len(predict_label)
+'''
+def get_train(myTable, epochs):
+    dataset = myTable.copy()
+    train_dataset = dataset.sample(frac=0.8, random_state=0)
+    test_dataset = dataset.drop(train_dataset.index)
+    train_features = train_dataset.copy()
+    test_features = test_dataset.copy()
+    train_labels = pd.DataFrame([train_features.pop('label')]).T
+    test_labels = pd.DataFrame([test_features.pop('label')]).T
+    normalizer = preprocessing.Normalization()
+    normalizer.adapt(np.array(train_features))
+    dnn_real_model = build_and_compile_model(normalizer)
+    history = dnn_real_model.fit(
+        train_features, train_labels,
+        validation_split=0.2,
+        epochs=epochs, verbose=0)
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    test_results = {}
+    test_results['signal'] = dnn_real_model.evaluate(
+        test_features,
+        test_labels, verbose=0)
+    probability_model = tf.keras.Sequential([dnn_real_model,
+                                             tf.keras.layers.Softmax()])
+
+    cf_ori = confusion_matrix(probability_model, test_features, test_labels)
+    return cf_ori, test_results
+
+def get_cross_training(myTable, epochs, cross_data):
+    train_dataset = training_set(myTable)
+    test_dataset = test_set(myTable, train_dataset)
+    train_features = train_dataset.copy()
+    test_features = test_dataset.copy()
+    train_labels = pd.DataFrame([train_features.pop('label')]).T
+    test_labels = pd.DataFrame([test_features.pop('label')]).T
+    normalizer = preprocessing.Normalization()
+    normalizer.adapt(np.array(train_features))
+    dnn_real_model = build_and_compile_model(normalizer)
+    history = dnn_real_model.fit(
+        train_features, train_labels,
+        validation_split=0.2,
+        epochs=epochs, verbose=0)
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    test_results = {}
+    test_results['signal'] = dnn_real_model.evaluate(
+        test_features,
+        test_labels, verbose=0)
+    probability_model = tf.keras.Sequential([dnn_real_model,
+                                             tf.keras.layers.Softmax()])
+
+    cf_ori = confusion_matrix(probability_model, test_features, test_labels)
+    cross_features = cross_data.copy()
+    cross_label  = pd.DataFrame([cross_features.pop('label')]).T
+    cf_cross = confusion_matrix(probability_model, cross_features, cross_label)
+    test_results['cross'] = dnn_real_model.evaluate(
+        cross_features,
+        cross_label, verbose=0
+    )
+    '''
+    hist['epoch'] = history.epoch
+    result1 = hist.tail()
+    result1 = result1.append(test_results, ignore_index=True)
+    hist.to_csv('./result/'+ test_time + '/' + files + '.csv', index=False)
+    '''
+    return [cf_ori, cf_cross, test_results]
+
+def confusion_matrix(model, feature, label):
+    expected = model.predict(feature)
+    expected = get_results(expected)
+    cf = tf.math.confusion_matrix(expected, label.to_numpy()).numpy()
+    cf = pd.DataFrame(cf)
+    return cf
+
+
+def plot_heatmap(ax, symbol_error, cons_error):
+    df = sns.heatmap(symbol_error, annot=True,cmap=plt.cm.Blues, ax=ax)
+    df.xaxis.tick_top()
+    df.set_xlabel(['P(cons error)= {:.2f}'.format(cons_error)])
+
+'''
+def plot_loss(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.ylim([0, 10])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error [Ireal]')
+    plt.legend()
+    plt.grid(True)
+'''
+
+def prediction(model, testTable, test_results, fileNanme):
+    test_features = testTable.copy()
+    test_labels = pd.DataFrame([test_features.pop('label')]).T
+    test_results[fileNanme] = model.evaluate(
+        test_features,
+        test_labels, verbose=0)
+
+def divide_Result(cf, file_name, test_time):
+    cons_4 = cf[:4]
+    cons_16 = cf[4:]
+    cons_4_error = (cons_4.iloc[:, 4:].sum().sum()) / cons_4.sum().sum()
+    cons_16_error = (cons_16.iloc[:, :4].sum().sum()) / cons_4.sum().sum()
+    symbol_error_results_4 = np.array([1 - cons_4.iloc[i, i] /
+                                       cons_4.iloc[i, :].sum() for i in range(4)]).reshape(2, 2)
+    symbol_error_results_16 = np.array(
+        [1 - cons_16.iloc[i - 4, i] /
+         cons_16.iloc[i - 4, :].sum() for i in range(4, 20)]).reshape(4, 4)
+    symbol_error_results_4 = pd.DataFrame(symbol_error_results_4)
+    symbol_error_results_16 = pd.DataFrame(symbol_error_results_16)
+    '''
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    plot_heatmap(ax1, symbol_error_results_4, cons_4_error)
+    plot_heatmap(ax2, symbol_error_results_16, cons_16_error)
+    plt.savefig("./result/"+ test_time+'/'+file_name, dpi=500)
+    '''
+
+
+def batch_result(cf):
+    array = []
+    cons_4 = cf[:4]
+    cons_16 = cf[4:]
+    for j in range(len(cf)):
+        result = cf.iloc[j, j] / cf.iloc[j, :].sum()
+        array.append(result)
+    cons_4_error = (cons_4.iloc[:, 4:].sum().sum()) / cons_4.sum().sum()
+    cons_16_error = (cons_16.iloc[:, :4].sum().sum()) / cons_16.sum().sum()
+    array.append(cons_4_error)
+    array.append(cons_16_error)
+    array = pd.DataFrame(array).T
+    return array
+
+def cross_test(train_set, test_set, ori, cross, test_result):
+    test_dataset = test_set.sample(frac=0.2, random_state=0)
+    cf_ori, cf_cross, result = get_cross_training(train_set, 20, test_dataset)
+    cf_ori_array = batch_result(cf_ori)
+    cf_cross_array = batch_result(cf_cross)
+    result = pd.DataFrame(result).T
+    ori = ori.append(cf_ori_array)
+    cross = cross.append(cf_cross_array)
+    test_result = test_result.append(result)
+    return [ori, cross, test_result]
+
+
+
