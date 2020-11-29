197265bf77fc321e140b89ab0a878523346650bd
lidonghao <leedonhom@gmail.com>
2020-11-28 17:28:57 -0800

    update 10.27

--- gan_module/cnn_gan_module.py
+++ gan_module/cnn_gan_module.py
@@ -183,7 +183,7 @@ if ckpt_manager.latest_checkpoint:
     ckpt.restore(ckpt_manager.latest_checkpoint)
     print ('Latest checkpoint restored!!')
 LAMBDA = 10
-EPOCHS = 50
+EPOCHS = 500
 data1 = "my_data"
 data1_label = "my_labels"
 data = dataset(data1, data1_label)
