e8f4151eb823954c4fb3b171a3ba7d092d3fab89
lidonghao <leedonhom@gmail.com>
2020-12-06 17:01:48 -0500

    update 10.27

--- gan_module/cnn_gan_module.py
+++ gan_module/cnn_gan_module.py
@@ -56,11 +56,12 @@ def table_data(my_data, cons, label, interference, noise, label_real, label_imag
 
 def make_generator():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(128, (1, 1), activation='relu', input_shape=(1, 50, 2)))
+    model.add(layers.Conv2D(32, (1, 1), activation='relu', input_shape=(1, 50, 2)))
     model.add(layers.MaxPooling2D((1, 1)))
     model.add(layers.Conv2D(64, (1, 1), activation='relu'))
     model.add(layers.MaxPooling2D((1, 1)))
-    model.add(layers.Conv2D(32, (1, 1), activation='relu'))
+    model.add(layers.Conv2D(64, (1, 1), activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(2))
     return model
 
