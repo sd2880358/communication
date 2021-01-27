b3457a797ff352229b9dce8ba4ecbf271b3a08ce
lidonghao <leedonhom@gmail.com>
2021-01-27 17:18:35 -0500

    update 10.27

--- gan_module/cycleGAN.py
+++ gan_module/cycleGAN.py
@@ -154,7 +154,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
             gen = (s + fake_u)
             s_hat = disentangle_t(gen)
             u_k = total - label
-            print(u_k.numpy())
+            print("test value of signal is",  s[1,1,1])
             result_fake_u = discriminator_u(fake_u, training=True)
             result_real_u = discriminator_u(u_k, training=True)
             fake_t = discriminator_t(gen, training=True)
