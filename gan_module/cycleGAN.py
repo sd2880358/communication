1e409be0d2b780254e8a8824ee3264c8924751a0
lidonghao <leedonhom@gmail.com>
2021-01-27 17:08:09 -0500

    update 10.27

--- gan_module/cycleGAN.py
+++ gan_module/cycleGAN.py
@@ -219,7 +219,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
         if epoch == EPOCHS - 1:
 
             ## measuring the absolute loss between generator and disentanglement
-            '''
+
             g_noise = tf.random.normal([BATCH_SIZE, blockSize, 2, 1])
             fake_c = generator_s(g_noise)
             fake_u = generator_u(g_noise)
@@ -227,11 +227,15 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
             fake_s = disentangle_t(fake_mixed)
             id_loss = abs(fake_s - fake_c).numpy().mean()
             relative_loss = np.median(abs((fake_s - fake_c) / fake_c))
+            print("----------")
+            print(fake_c[1,1,1])
+            print(fake_u[1,1,1])
+            print("-----")
             '''
             fake_c = disentangle_t(feature)
             id_loss = abs(fake_c - labels).numpy().mean()
             relative_loss = np.median(abs((labels - fake_c) / labels))
-            '''
+            
             sample = tf.random.normal([1000, blockSize, 2, 1])
             fake_s = generator_s(sample)
             fake_i = generator_i(sample)
@@ -263,7 +267,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
 
 
 if __name__ == '__main__':
-    EPOCHS = 100
+    EPOCHS = 1
     LAMBDA = 10
     date = "1_27/"
     generator_s_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
