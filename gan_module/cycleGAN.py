3272476f65215019555dd74891572ac1c4bb586c
lidonghao <leedonhom@gmail.com>
2021-01-27 19:15:54 -0500

    update 10.27

--- gan_module/cycleGAN.py
+++ gan_module/cycleGAN.py
@@ -234,7 +234,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
             print("actual feature", feature[1,1,1])
             print("actual labels", labels[1,1,1])
             test = discriminator_d(labels)
-            print(generator_loss(labels))
+            print(test.mean())
             fake_s = disentangle_t(feature)
 
             # relative loss between fake signal and signal_hat
