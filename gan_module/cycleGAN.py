c8162aa45dda63bbdab3aed0bb584df6900575ee
lidonghao <leedonhom@gmail.com>
2021-01-27 19:18:21 -0500

    update 10.27

--- gan_module/cycleGAN.py
+++ gan_module/cycleGAN.py
@@ -276,7 +276,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
 
 
 if __name__ == '__main__':
-    EPOCHS = 10
+    EPOCHS = 300
     LAMBDA = 10
     date = "1_27/"
     generator_s_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
