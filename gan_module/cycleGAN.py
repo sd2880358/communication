a31a59d3daa73aeb63d5d5cb9555aaad01ba64a6
lidonghao <leedonhom@gmail.com>
2021-01-31 18:16:17 -0500

    update 10.27

--- gan_module/cycleGAN.py
+++ gan_module/cycleGAN.py
@@ -325,5 +325,5 @@ if __name__ == '__main__':
         modify = data.loc[:, ["fake_real", "fake_imag", "block"]]
         qam = data.loc[:, ["cons"]]
         label = data.loc[:, ["labels"]]
-        cls.qam_training(modify, qam, 50, 100, "test1_qam")
-        cls.symbol_training(modify, label, 50, 300, "test1_symbol")
+        cls.qam_training(modify, qam, 50, 100, "test2_qam")
+        cls.symbol_training(modify, label, 50, 300, "test2_symbol")
