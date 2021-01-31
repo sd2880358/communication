197bd28ba6dfcf2267116b22646eebf5872df304
lidonghao <leedonhom@gmail.com>
2021-01-31 17:02:04 -0500

    update 10.27

--- gan_module/cycleGAN.py
+++ gan_module/cycleGAN.py
@@ -247,7 +247,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
             test = discriminator_d(labels)
             print(test.numpy().mean())
             fake_s = disentangle_t(feature)
-            print(disen_hist)
+            print("the tensor loss is ", disen_hist)
             test_hist = np.array(disen_hist)
             result = pd.DataFrame(
                   {
