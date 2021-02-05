483902ecc1e8057dd74271c43c55a89188782dca
lidonghao <leedonhom@gmail.com>
2021-02-04 21:08:38 -0500

    update 10.27

--- gan_module/d_autoencoder.py
+++ gan_module/d_autoencoder.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, losses
 from tensorflow.keras.datasets import fashion_mnist
