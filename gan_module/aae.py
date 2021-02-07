6ae6297487e24c3e41f077c7ca5589b08fae8154
lidonghao <leedonhom@gmail.com>
2021-02-07 16:38:33 -0500

    update 10.27

--- gan_module/aae.py
+++ gan_module/aae.py
@@ -274,7 +274,7 @@ def start_train(BATCH_SIZE, BUFFER_SIZE, data, filePath):
 
 
 if __name__ == '__main__':
-    EPOCHS = 1
+    EPOCHS = 100
     LAMBDA = 10
     date = "2_7/"
     generator_s_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
